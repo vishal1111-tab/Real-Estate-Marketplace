27ecfa532aecb100235dcedf97b03bf9f88892cf
vishal kumar <vishalg1938@gmail.com>
2024-07-23 22:52:48 +0530

    Complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utilis/error.js";
-
+import bcryptjs from 'bcryptjs';
 export const test =  (req , res) => {
     res.json({
         "msg" : "Hello world"
