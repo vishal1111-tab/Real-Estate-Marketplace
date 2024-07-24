ec5c76861ce619c614c3c3e40fd3ca08d284a6b3
vishal kumar <vishalg1938@gmail.com>
2024-07-24 21:18:14 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import  listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -24,8 +25,8 @@ app.listen(3000 , () => {
 
 
 app.use('/api/user' , userRouter );
-
 app.use('/api/auth' , authRouter);
+app.use('/api/listing' , listingRouter);
 
 
 app.use((err, req , res ,next ) => {
--- api/utilis/verifyUser.js
+++ api/utilis/verifyUser.js
@@ -8,10 +8,8 @@ export const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
-    
-    console.log(user);
+
     req.user = user;
-   
     next();
   });
 };
