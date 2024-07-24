f9209395721eb2ac40b6bf104de1cfa60aceab4d
vishal kumar <vishalg1938@gmail.com>
2024-07-24 21:55:17 +0530

    complete CreateListing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,6 +20,7 @@ import {
   updateUserSuccess,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 function Profile() {
   const { currentUser, error, loading } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
@@ -186,6 +187,11 @@ function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to = {"/create-listing"} >
+          Create Listing
+        </Link>
+
       </form>
       <div className="flex justify-between mt-5">
         <span
